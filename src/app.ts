9b178202700909df8b80b84149884c6dbbee6026
azimzimmer <zimmerat@gmail.com>
2025-12-18 00:19:02 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from 'express';
 import tasksRoutes from './routes/tasks.routes';
+import cors from "cors";
+
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+
 app.use(express.json());
 
 app.use('/tasks', tasksRoutes);
